7865c1c15f9621e0f0c4c83e309b0088bc43107b
Feliphe Blatt <felipheblatt@gmail.com>
2025-04-06 11:46:56 -0300

    .

---
+++ untitled/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+  public static void main(String[] args) {
+    int a, b, c;
+    a = 5;
+    b = 10;
+    c = a + b;
+    
+    System.out.println("Sum of a and b is: " + c);
+    
+    b = 20;
+    System.out.println("Sum of a and b is: " + c);
+    
+    c = a + b;
+    System.out.println("Sum of a and b is: " + c);
+    
+  }
+}
